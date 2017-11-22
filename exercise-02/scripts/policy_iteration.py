b43166d7449344ba526d49dec3d491134da2d0f0
Hendrik Vloet <vloeth@tfpool22.informatik.uni-freiburg.de>
2017-11-22 17:07:18 +0100

    created new branch for me

--- exercise-02/scripts/policy_iteration.py
+++ exercise-02/scripts/policy_iteration.py
@@ -23,7 +23,7 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
 		
 	"""
 	# define action set
-	actions = np.arange(0,4)
+	actions = np.arange(env.nA)
 	# initialize V(s) with zeros
 	V = np.zeros(env.nS)
 	# learning loop
@@ -40,12 +40,104 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
 									*V[env.P[state][action][0][1]])
 		# compare delta to parameter theta, if small enough -> break
 		if np.sum(np.abs(V-v_new)) < theta:
-			#print("Random policy")
-			#print(np.round(v_new))
 			break
 		# update old value function
 		V = v_new
-	return np.array(V)
+	return np.array(np.round(V))
+
+
+def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=1.0):
+	"""
+	Policy Improvement Algorithm. Iteratively evaluates and improves a policy
+	until an optimal policy is found.
+
+	Args:
+	env: The OpenAI envrionment.
+	policy_eval_fn: Policy Evaluation function that takes 3 arguments:
+	policy, env, discount_factor.
+	discount_factor: Lambda discount factor.
+
+	Returns:
+	A tuple (policy, V). 
+	policy is the optimal policy, a matrix of shape [S, A] where each state s
+	contains a valid probability distribution over actions.
+	V is the value function for the optimal policy.
+
+	"""
+	
+	# Start with a random policy, initialize V
+	V = np.zeros(env.nS)
+	policy = np.ones([env.nS, env.nA]) / env.nA
+	actions = np.arange(env.nA)
+	states = np.arange(env.nS)
+	
+	
+	
+	print("new value function: {}".format(V))
+	#while True:
+	for i in range(0,5):
+	# Implement this!
+		V=policy_eval_fn(policy,env)
+		print(V)
+		POLICY_STABLE = True
+		old_policy = policy
+		#print("old policy: {}".format(old_policy))
+		policy_stable = True
+		for state in states:
+			candidates = []
+			#print("V of state {}: {}".format(state, V[state]))
+			for action in actions:
+				candidates.append(V[env.P[state][action][0][1]])
+			#print("candidates of state {}: {}".format(state,candidates))
+			max_idx = np.where(candidates == np.max(candidates))
+			#print("indices of max values in state {}: {}".format(state,max_idx))
+			policy[state] = 0
+			policy[state][max_idx] = 1/len(max_idx[0])
+			del max_idx
+			#print("new greedy policy in state {}: {}".format(state,policy[state]))
+		print("new greedy policy: {}".format(policy))
+	
+		#V=policy_eval_fn(policy,env)
+		#print(V)
+		#print(policy)	
+	return policy,V
+	
+	
+	
+	
+	
+		#if not (np.array_equal(old_policy, policy)):
+			#policy_stable = False
+		#if policy_stable == True:
+			#break
+		#else:
+			#V = policy_eval_fn(policy,env)
+		
+		#return policy, V
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+##### CODE DUMPSTER ########################################################################
+
+
+
+
 
 
 def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=1.0):
@@ -104,3 +196,4 @@ def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=1.0):
 
 	return policy, V
 
+'''
