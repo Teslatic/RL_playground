ca14b79e6c510ff919e3d9d4fca55fe9dc46d494
Nico <nico.ott@outlook.com>
2018-02-17 17:13:58 +0100

    Removed more files

--- project/nico/temp/lior/assets/helperFunctions/discretize.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def discretize(limit_high, limit_low, D_action):
-    """
-    Method which is used to discretize a continous space.
-    Returns a vector with the discretized space.
-    """
-    return np.linspace(limit_low, limit_high, D_action)
--- project/nico_backup_old/code backups/nico_working_backup_1/assets/helperFunctions/discretize.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def discretize(limit_high, limit_low, N_action):
-    """
-    Method which is used to discretize a continous space.
-    Returns a vector with the discretized space.
-    """
-    return np.linspace(limit_low, limit_high, N_action)
